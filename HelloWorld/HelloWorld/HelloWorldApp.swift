20de9e7b6c523a3bb316e13a36e0f22faa869863
ProfessorScranton <jeff.scranton@icloud.com>
2025-05-15 14:23:13 -0500

    Hello World

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Jeffrey Scranton on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
